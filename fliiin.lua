119805bd5440f9bb4af32ee69859a06baedfc05e
Evan Johnson <ev.jonnson@gmail.com>
2025-08-31 16:08:41 -0600

    midi clocking working
    next: documentation

--- fliiin.lua
+++ fliiin.lua
@@ -8,11 +8,13 @@
 -- midi note velocity
 vel = 127
 default_clock_mode = "auto" -- "auto", "internal", or "midi"
+default_internal_bpm = 120 -- 2-198, MUST BE EVEN (config UI only supports even internal bpms)
 
 -- up to 16 banks
 note_bank = 1 -- initial bank
 note_banks = {}
 note_banks[1] = { 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24, 26, 28, }
+note_banks[4] = { 38, 40, 41, 43, 45, 47, 48, 50, 52, 53, 55, 57, 59, 60, 62, 64, }
 -- ascending fourths
 note_banks[2] = { 24, 29, 34, 39, 44, 49, 54, 59, 64, 69, 74, 79, 84, 89, 94, 99, }
 -- ascending fifths
@@ -22,56 +24,6 @@ note_banks[3] = { 12, 19, 26, 33, 40, 47, 54, 61, 68, 75, 82, 89, 96, 103, 110, 
 -- configurable at runtime, but no on device preset saving yet
 chans = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, }
 -- chans = { 1, 1, 1, 1, 1, 1, 1, 1, 1,  1,  1,  1,  1,  1,  1,  1, }
-
-local function note_on(col)
-  midi_note_on(col.note + transpose + 12 * octave, col.vel, col.ch)
-end
-
-local function note_off(col)
-  midi_note_off(col.note + transpose + 12 * octave, col.vel, col.ch)
-end
-
-local function tick_col(col)
-  col.div_tick = (col.div_tick + 1) % col.div
-
-  if col.div_tick == 0 then
-    col.pos = (col.pos + 1) % height
-  end
-
-  if col.pos - col.len <= 0 then
-    if not col.on then
-      note_on(col)
-      col.on = true
-    end
-  else
-    if col.on then
-      note_off(col)
-      col.on = false
-    end
-  end
-end
-
-local function stop_col(col)
-  if col.on then
-    note_off(col)
-  end
-
-  col.on = false
-  running_cols[col.x] = nil
-end
-
-local function start_col(col, div, len)
-  if running_cols[col.x] then
-    stop_col(col)
-  end
-  col.div = div
-  col.len = len
-  col.div_tick = div-1
-  col.pos = height
-  col.keys.div.y = nil
-  col.keys.len.y = nil
-  running_cols[col.x] = col
-end
 
 local function draw_col(col)
   local x = col.x
@@ -139,6 +91,92 @@ local function redraw()
   grid_refresh()
 end
 
+local function note_on(col)
+  midi_note_on(col.note + transpose + 12 * octave, col.vel, col.ch)
+end
+
+local function note_off(col)
+  midi_note_off(col.note + transpose + 12 * octave, col.vel, col.ch)
+end
+
+local function tick_col(col)
+  col.div_tick = (col.div_tick + 1) % col.div
+
+  if col.div_tick == 0 then
+    col.pos = (col.pos + 1) % height
+  end
+
+  if col.pos - col.len <= 0 then
+    if not col.on then
+      note_on(col)
+      col.on = true
+    end
+  else
+    if col.on then
+      note_off(col)
+      col.on = false
+    end
+  end
+end
+
+local function stop_col(col)
+  if col.on then
+    note_off(col)
+  end
+
+  col.on = false
+  running_cols[col.x] = nil
+end
+
+local function start_col(col, div, len)
+  if running_cols[col.x] then
+    stop_col(col)
+  end
+  col.div = div
+  col.len = len
+  col.div_tick = div-1
+  col.pos = height
+  col.keys.div.y = nil
+  col.keys.len.y = nil
+  running_cols[col.x] = col
+end
+
+local function tick()
+  -- print("tick")
+  for _,col in pairs(running_cols) do
+    tick_col(col)
+  end
+
+  if not config_page.active then
+    redraw()
+  end
+end
+
+local function internal_tick()
+  tick()
+
+  if clock.dirty then
+    start_internal_ticker(bpm_to_ms(clock.internal_bpm))
+    clock.dirty = false
+  end
+end
+
+local function midi_tick()
+  tick()
+end
+
+local function start_internal_ticker(time)
+  metro.stop(clock.ticker)
+  clock.internal_ticker_ms = math.floor(time / clock.internal_calculated_time_div)
+  clock.ticker = metro.new(internal_tick, clock.internal_ticker_ms)
+end
+
+local function start_midi_ticker(time)
+  metro.stop(clock.ticker)
+  clock.midi_ticker_ms = math.floor(time / clock.midi_calculated_time_div)
+  clock.ticker = metro.new(midi_tick, clock.midi_ticker_ms, 4*2^(clock.midi_divider))
+end
+
 local function reset_cols(cols)
   for x,col in pairs(cols) do
     col.pos = height
@@ -203,12 +241,69 @@ local function change_octave(oct)
   end
 end
 
-local function midi_tick(d1,d2,d3,d4)
+local function bpm_to_ms(bpm)
+  return math.floor(60 / bpm / 16 * 1000)
+end
+
+local function avg(t)
+  local sum = 0
+  local n = #t
+
+  for i=1,n do
+    sum = sum + t[i]
+  end
+
+  return sum / n
+end
+
+-- given time in ms of a 16th note, return the bpm
+local function sixteenth_to_bpm(ms)
+  local bpm = 60000 / (ms * 4)
+  return math.floor(bpm * 100) / 100
+end
+
+-- utility to help with user-defined default internal bpm
+local function bpm_to_coarse_fine(bpm)
+  if bpm % 2 ~= 0 or bpm < 2 or 198 < bpm then
+    error("bpm must be even integer in range 2-198, was "..bpm, 2)
+  end
+
+  local coarse = math.floor(bpm/20)
+  local fine =  math.floor((bpm - coarse*20) / 2)
+
+  return coarse+1,fine+1
+end
+
+local function midi_sync(d1,d2,d3,d4)
   clock.midi_received = true
 
   if d1==8 and d2==240 then
-    ticks = ((ticks + 1) % 12)
-    if ticks == 0 and midi_clock_in then tick() end
+    local ticks = clock.midi_div_ticks
+    ticks = ((ticks + 1) % 24)
+    clock.midi_div_ticks = ticks
+
+    -- midi is 24 ppq, track tempo every 16th note
+    if ticks % 6 == 0 then
+      local current_time = get_time()
+
+      if clock.midi_clock_last_time then
+        local elapsed_time = current_time - clock.midi_clock_last_time
+        table.insert(clock.midi_clock_times, 1, elapsed_time)
+        clock.midi_clock_times[5] = nil
+      end
+
+      clock.midi_clock_last_time = current_time
+    end
+
+    -- sync internal ticker every quarter note
+    if ticks == 0 then
+      metro.stop(clock.ticker)
+
+      local sixteenth_ms = avg(clock.midi_clock_times)
+      clock.midi_bpm = sixteenth_to_bpm(sixteenth_ms)
+
+      start_midi_ticker(sixteenth_ms/4)
+    end
   else
     -- ps("midi_rx %d %d %d %d",d1,d2,d3,d4)
   end
@@ -220,9 +315,6 @@ end
 
 local function midi_await(d1,d2,d3,d4)
   if d1==8 and d2==240 then
-    -- ticks = ((ticks + 1) % 12)
-    -- if ticks == 0 and midi_clock_in then tick() end
-
     clock.source = "midi"
     update_clock(clock)
 
@@ -241,11 +333,9 @@ local function midi_timeout_check()
     if config_page.active then
       redraw()
     end
+  else
+    clock.midi_received = false
   end
-end
-
-function bpm_to_ms(bpm)
-  return math.floor(60 / bpm / 32 * 1000)
 end
 
 function update_clock(clock)
@@ -255,39 +345,43 @@ function update_clock(clock)
         clock.midi_timeout_check = metro.new(midi_timeout_check, 3000)
       end
 
-      midi_rx = midi_tick
+      midi_rx = midi_sync
     else
       midi_rx = midi_await
     end
   elseif clock.mode == "internal" then
     clock.source = "internal"
+    midi_rx = midi_ignore
   elseif clock.mode == "midi" then
     clock.source = "midi"
   end
 
   if clock.source == "internal" then
     local bpm =  0 + 20 * (clock.coarse - 1) + 2 * (clock.fine - 1)
-    bpm = bpm * (1/4 * 2^(clock.internal_divider))
-    bpm = math.max(1, bpm)
 
-    if bpm ~= clock.bpm then
-      clock.bpm = bpm
+    if not clock.internal_bpm then -- first internal clock since startup
+      clock.internal_bpm = bpm
+      clock.dirty = false
+      start_internal_ticker(bpm_to_ms(bpm))
+    elseif clock.midi_clock_times then -- switching to internal clock from midi
+      clock.midi_clock_times = nil -- indicates not following midi clock
+      clock.internal_bpm = bpm
+      clock.dirty = false
+      start_internal_ticker(bpm_to_ms(bpm))
+    elseif clock.internal_ticker_ms ~= math.floor(bpm_to_ms(bpm) / clock.internal_calculated_time_div) then
+      clock.internal_bpm = bpm
       clock.dirty = true
     end
-
-    if not clock.ticker then
-      clock.ticker = metro.new(tick, bpm_to_ms(bpm))
-      clock.dirty = false
-    end
   elseif clock.source == "midi" then
-    clock.bpm = nil
-
-    if clock.ticker then
-      metro.stop(clock.ticker)
-      clock.ticker = nil
+    clock.internal_bpm = nil
+    -- switching to midi clock from internal clock (or startup on midi clock)
+    if not clock.midi_clock_times then
+      clock.midi_clock_times = {}
+      clock.midi_clock_last_time = nil
+      start_midi_ticker(bpm_to_ms(clock.midi_bpm))
     end
 
-    midi_rx = midi_tick
+    midi_rx = midi_sync
   end
 end
 
@@ -314,7 +408,10 @@ grid = function(x,y,z)
         elseif x >= 12 then
           if clock.source == "internal" then
             clock.internal_divider = x-12
+            clock.internal_calculated_time_div = 1/4 * 2^(clock.internal_divider)
           elseif clock.source == "midi" then
+            clock.midi_divider = x-12
+            clock.midi_calculated_time_div = 1/4 * 2^(clock.midi_divider)
           end
         end
 
@@ -410,23 +507,6 @@ grid = function(x,y,z)
   redraw()
 end
 
-tick = function()
-  -- print("tick")
-  for _,col in pairs(running_cols) do
-    tick_col(col)
-  end
-
-  if not config_page.active then
-    redraw()
-  end
-
-  if clock.dirty then
-    metro.stop(clock.ticker)
-    clock.ticker = metro.new(tick, bpm_to_ms(clock.bpm))
-    clock.dirty = false
-  end
-end
-
 local function init()
   -- validate note and channel banks
   for n,bank in pairs(note_banks) do
@@ -483,12 +563,20 @@ local function init()
   end
 
   clock ={
-    coarse = 7, -- 1-10
-    fine = 1, -- 1-12
+    coarse = 1, -- 1-10
+    fine = 1, -- 1-10
     mode = default_clock_mode, -- "auto", "internal", "midi"
     internal_divider = 2, -- 0-4, 2 is center, adjacent steps mult/div by 2
-    midi_divider = 2
+    midi_divider = 2,
+    midi_div_ticks = 0,
+    midi_bpm = 120, -- must be initialized, updated as soon as tempo is calculated
+    ticker = 100, -- dummy metro id, must be initialized
   }
+  clock.internal_calculated_time_div = 1/4 * 2^(clock.internal_divider)
+  clock.midi_calculated_time_div = 1/4 * 2^(clock.midi_divider)
+
+  clock.coarse,clock.fine = bpm_to_coarse_fine(default_internal_bpm)
+
   if clock.mode == "auto" then
     clock.source = "internal"
   end
